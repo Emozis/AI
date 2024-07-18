0a16b7559d18abb18a1cb29b01aad1f7c05b6612
narae3759 <narae3759@ppsystem.co.kr>
2024-07-18 11:06:04 +0900

    Feat: Sidebar 기능 추가(테스트)

--- Home.py
+++ Home.py
@@ -1,8 +1,12 @@
 # python -m streamlit run Home.py
 import streamlit as st 
 from naraetool.style import *
+from st_pages import show_pages_from_config
 
 setting()
+
+show_pages_from_config()
+
 
 read_mdfile("./docs/readme.md")
     
---
+++ pages/Test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+create_page = st.Page("create.py", title="Create entry", icon=":material/add_circle:")
+delete_page = st.Page("delete.py", title="Delete entry", icon=":material/delete:")
+
+pg = st.navigation([create_page, delete_page])
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+pg.run()
