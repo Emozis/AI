e4c1b34c136fa24e1ed2b1aa6e8f33e98141a1c0
narae3759 <narae3759@ppsystem.co.kr>
2024-07-18 12:58:40 +0900

    Feat: LinkTree 기능 추가
    - 홈페이지를 Linktree로 변경
    - 기존의 Ideas를 삭제
    - 프로젝트 소개 페이지를 추가

--- Home.py
+++ Home.py
@@ -1,11 +1,49 @@
 # python -m streamlit run Home.py
 import streamlit as st 
 from naraetool.style import *
-from st_pages import show_pages_from_config
+from st_pages import show_pages_from_config, add_page_title
 
 setting()
 
+# Sidebar
 show_pages_from_config()
+add_page_title()
 
-read_mdfile("./docs/readme.md")
+# LinkTree
+st.link_button(
+    label=":blue[💡 프로젝트 소개]",
+    url="https://meta-persona-ai.streamlit.app/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%20%EC%86%8C%EA%B0%9C",
+    use_container_width=True,
+    # type="primary"
+)
+
+st.link_button(
+    label=":blue[📋 2024.07.15 회의록]",
+    url="https://meta-persona-ai.streamlit.app/%ED%9A%8C%EC%9D%98%EB%A1%9D",
+    use_container_width=True,
+    # type="primary"
+)
+
+st.link_button(
+    label=":blue[🔗 Google Drive 바로 가기]",
+    url="https://drive.google.com/drive/folders/1SZaUQ8dmTElSgmZb7Cfk4XAHWrqpHK7y?usp=drive_link",
+    use_container_width=True,
+    # type="primary"
+)
+
+st.link_button(
+    label=":blue[🔗 GitHub 바로 가기]",
+    url="https://github.com/meta-persona-ai",
+    use_container_width=True,
+    # type="primary"
+)
+
+st.link_button(
+    label=":blue[🔗 Discord 바로 가기]",
+    url="https://discord.com/channels/1261648325648846949/1261648326239981571",
+    use_container_width=True,
+    # type="primary"
+)
+
+
     
