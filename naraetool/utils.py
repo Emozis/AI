4cc09df2050e9916929a6adc15d1fbd5cafa2c69
narae3759 <narae3759@gmail.com>
2024-07-26 00:04:54 +0900

    - Refact: 프롬프트 읽는 함수 생성
    - 코드 변경이 많은 Demo.py의 경우 충돌의 위험이 있어,
        코드에 접근하지 않고 프롬프트를 수정할 수 있는 방법을 생각
    - 프롬프트는 항상 static/templates에 저장하도록 한다.

--- naraetool/utils.py
+++ naraetool/utils.py
@@ -10,8 +10,8 @@ def setting():
     read_mdfile("./docs/css.md")
 
 
-def read_mdfile(filepath:str) -> str:
-    """markdown 파일을 읽고 텍스트로 반환하는 함수
+def read_mdfile(filepath:str) -> st.markdown:
+    """markdown 파일을 읽고 markdown으로 작성하는 함수
 
     Args:
         filepath (str): markdown 파일 경로
@@ -27,4 +27,23 @@ def read_mdfile(filepath:str) -> str:
         file_text = file.read_text(encoding="utf-8")
 
     return st.markdown(file_text, unsafe_allow_html=True)
+
+
+def read_prompt(filepath:str) -> str:
+    """프롬프트 파일을 읽고 텍스트로 반환하는 함수
+
+    Args:
+        filepath (str): markdown 파일 경로
+
+    Returns:
+        str: markdown 파일에서 추출된 텍스트
+    """
+    file = Path(filepath)
+    
+    if not file.is_file():
+        file_text = f"[ERROR] 파일 경로를 찾을 수 없습니다.(INPUT PATH: {filepath})"
+    else:
+        file_text = file.read_text(encoding="utf-8")
+
+    return file_text
 
